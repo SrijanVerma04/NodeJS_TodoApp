60d62f3f0a8b53cac302a64366ffaeab0aa3523d
Srijan Verma <srijanverma102@gmail.com>
2023-10-02 15:28:03 +0530

    minor changes in the user.js file

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/feature.js";
 import ErrorHandler from "../middlewares/error.js";
 
-export const register = async(req,res) => {
+export const register = async(req,res , next) => {
 
     try {
         const {name , email, password} = req.body;
@@ -26,7 +26,7 @@ export const register = async(req,res) => {
     }  
 };
 
-export const login = async(req,res) => {
+export const login = async(req,res , next) => {
 
     try {
         const {email , password} = req.body;
